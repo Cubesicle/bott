97d142f00a907155d16bcd1f72eb9766ac65c9c3
Ott Boonpirom <ottboonpirom@gmail.com>
2023-02-03 08:50:57 -0600

    Eject dll when done

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,21 @@
 use std::ffi::c_void;
-use windows::Win32::{ Foundation::HINSTANCE, System::SystemServices::* };
+use windows::Win32::{ 
+    Foundation::HINSTANCE,
+    System::LibraryLoader::FreeLibraryAndExitThread,
+    System::SystemServices::*,
+};
+
+fn main_thread(hinst_dll: HINSTANCE) {
+    println!("yippee!!");
+    unsafe { FreeLibraryAndExitThread(hinst_dll, 0); }
+}
 
 #[no_mangle]
-pub extern "stdcall" fn DllMain(_: HINSTANCE, reason: u32, _: *mut c_void) -> bool {
+extern "system" fn DllMain(hinst_dll: HINSTANCE, reason: u32, _: *mut c_void) -> bool {
     match reason {
         DLL_PROCESS_ATTACH => {
-            unsafe {
-                windows::Win32::System::Console::AllocConsole();
-            }
-            println!("dll has been injected!!!!");
+            unsafe { windows::Win32::System::Console::AllocConsole(); } // TODO remove
+            std::thread::spawn(move || { main_thread(hinst_dll); });
             true
         },
         DLL_PROCESS_DETACH => true,
