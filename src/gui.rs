8e6e1e1a15be62658c953bfcc11a1a6626c4d0a1
Ott Boonpirom <ottboonpirom@gmail.com>
2023-02-24 23:47:47 -0600

    Add gui

--- src/gui.rs
+++ src/gui.rs
@@ -1,1 +1,13 @@
+use egui_opengl_internal::OpenGLApp;
 
+pub fn update(ctx: &egui::Context, _: &mut i32) {
+    egui::containers::Window::new("RBot").show(ctx, |ui| {
+        ui.heading("Hello World!");
+        ui.separator();
+        if ui.button("exit").clicked() {                
+            unsafe { crate::EXITING = true; }
+        }
+    });
+}
+
+pub static mut APP: OpenGLApp<i32> = OpenGLApp::new();
