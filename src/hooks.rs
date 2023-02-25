ef0a0c5623281ceab3057d6fc61aacdfae5a1565
Ott Boonpirom <ottboonpirom@gmail.com>
2023-02-24 23:48:09 -0600

    Gui stuff

--- src/hooks.rs
+++ src/hooks.rs
@@ -1,9 +1,18 @@
+use crate::gui;
+
 use anyhow::{bail, Context, Result};
 use detour::static_detour;
+use windows::Win32::Foundation::{HWND, WPARAM, LPARAM, LRESULT};
 use std::ffi::CString;
+use std::mem::transmute;
+use std::os::raw::c_void;
+use std::sync::Once;
 use windows::core::{PCSTR, PCWSTR};
-use windows::Win32::Graphics::Gdi::HDC;
+use windows::Win32::Graphics::Gdi::{HDC, WindowFromDC};
 use windows::Win32::System::LibraryLoader::{GetModuleHandleW, GetProcAddress};
+use windows::Win32::UI::WindowsAndMessaging::{GWLP_WNDPROC, WNDPROC, SetWindowLongPtrA, CallWindowProcW};
+
+static mut OLD_WND_PROC: Option<WNDPROC> = None;
 
 static_detour! {
     static WGLSwapBuffersHook: extern "system" fn(HDC);
@@ -24,16 +33,63 @@ pub fn load() -> Result<()> {
 }
 
 pub fn unload() -> Result<()> {
-    unsafe { WGLSwapBuffersHook.disable()?; }
+    unsafe {
+        WGLSwapBuffersHook.disable()?;
+    }
+
+    unsafe {
+        let wnd_proc = OLD_WND_PROC.unwrap().unwrap();
+        let _: Option<WNDPROC> = Some(transmute(SetWindowLongPtrA(
+            gui::APP.get_window(),
+            GWLP_WNDPROC,
+            wnd_proc as usize as _,
+        )));
+    }
+
     Ok(())
 }
 
 fn wgl_swap_buffers_detour(hdc: HDC) {
-    println!("buffer be swapping");
-    WGLSwapBuffersHook.call(hdc);
+    unsafe {
+        static INIT: Once = Once::new();
+        INIT.call_once(|| {
+            println!("wglSwapBuffers successfully hooked.");
+
+            let window = WindowFromDC(hdc);
+            gui::APP.init_default(hdc, window, gui::update);
+
+            OLD_WND_PROC = Some(transmute(SetWindowLongPtrA(
+                window,
+                GWLP_WNDPROC,
+                call_wnd_proc_detour as usize as _,
+            )));
+        });
+
+        gui::APP.render(hdc);
+        WGLSwapBuffersHook.call(hdc);
+    }
 }
 
-fn get_module_symbol_address(module: &str, symbol: &str) -> Result<usize> {
+unsafe extern "stdcall" fn call_wnd_proc_detour(
+    hwnd: HWND,
+    msg: u32,
+    wparam: WPARAM,
+    lparam: LPARAM,
+) -> LRESULT {
+    static INIT: Once = Once::new();
+    INIT.call_once(|| {
+        println!("CallWindowProcW successfully hooked.");
+    });
+
+    let egui_wants_input = gui::APP.wnd_proc(msg, wparam, lparam);
+    if egui_wants_input {
+        return LRESULT(1);
+    }
+
+    CallWindowProcW(OLD_WND_PROC.unwrap(), hwnd, msg, wparam, lparam)
+}
+
+pub fn get_module_symbol_address(module: &str, symbol: &str) -> Result<*const c_void> {
     let module_utf16 = module
         .encode_utf16()
         .chain(std::iter::once(0))
@@ -45,7 +101,7 @@ fn get_module_symbol_address(module: &str, symbol: &str) -> Result<usize> {
             handle,
             PCSTR::from_raw(symbol_ansi.to_bytes_with_nul().as_ptr()),
         ) {
-            Ok(func as usize)
+            Ok(func as *const c_void)
         } else {
             bail!(format!("Could not get memory address of {} in {}", symbol, module))
         }
